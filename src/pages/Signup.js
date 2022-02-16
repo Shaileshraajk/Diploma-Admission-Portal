2237b09ed4a5e47d75eae5675ba88bb7644465db
Shaileshraajk <shailesh.karthik16@gmail.com>
2022-02-16 14:47:24 +0530

    SignupButton Updated

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,10 @@
 import {StyledTextInput, StyledFormArea, StyledFormButton, StyledLabel, Avatar, StyledTitle, colors, ButtonGroup, ExtraText, TextLink, CopyrightText} from './../components/Styles'
 import RambosLogo from './../assets/RambosLogo.jpg';
-import {Formik, Form} from 'formik';
+import {Formik, Form, Field} from 'formik';
 import {TextInput} from './../components/FormLib';
 import {FiMail, FiLock, FiUser, FiSmartphone} from 'react-icons/fi';
 import * as Yup from 'yup';
 import "yup-phone";
-import {Rings} from 'react-loader-spinner';
 import React from 'react';
 
 const Signup = () => {
@@ -16,34 +15,42 @@ const Signup = () => {
                 <StyledTitle color={colors.theme} size={30}>Signup</StyledTitle>
                 <Formik
                     initialValues={{
+                        user_type: "",
                         email: "",
-                        password: "",
-                        confirmPassword: "",
-                        mobileNumber: "",
-                        username: "",
-                        usertype: ""
+                        name: "",
+                        mobno: "",
+                        pwd: "",
+                        confirmpwd: ""
                     }}
                     validationSchema={
                         Yup.object({
-                            usertype: Yup.string().required("Required"),
+                            user_type: Yup.string().required("Required"),
                             email: Yup.string().email("Invalid email id")
                                 .required("Required"),
-                            password: Yup.string().min(8, "Password is too short").max(15, "Password is too long")
+                            pwd: Yup.string().min(8, "Password is too short").max(15, "Password is too long")
                                 .required("Required"),
-                            username: Yup.string().required("Required"),
-                            mobileNumber: Yup.string().phone('IN', true, 'Mobile Number is invalid'),
-                            confirmPassword: Yup.string().required("Required").oneOf([Yup.ref("password")], "Passwords do not match")
+                            name: Yup.string().required("Required"),
+                            mobno: Yup.string().phone('IN', true, 'Mobile Number is invalid'),
+                            confirmpwd: Yup.string().required("Required").oneOf([Yup.ref("pwd")], "Passwords do not match")
                         })
                     }
-                    onSubmit={(values, {setSubmitting}) => {
-                        console.log(values);
+                    onSubmit={(fields) => {
+                        console.log(JSON.stringify(fields, null, 4));
+                        fetch("http://localhost:8080/user/add",{
+                            method:"POST",
+                            headers:{"Content-Type":"application/json"},
+                            body:JSON.stringify(fields)
+
+                            }).then(()=>{
+                                console.log("New user added")
+                            })
                     }}
                 >
                     {({isSubmitting}) => (
                         <Form>
 
                             <TextInput
-                                name="usertype"
+                                name="user_type"
                                 type="text"
                                 label="User Type -  1 for Admin, 2 for User"
                                 placeholder="1 or 2"
@@ -51,7 +58,7 @@ const Signup = () => {
                             />
 
                             <TextInput
-                                name="username"
+                                name="name"
                                 type="text"
                                 label="Full Name"
                                 placeholder="Daniel"
@@ -59,7 +66,7 @@ const Signup = () => {
                             />
 
                             <TextInput
-                                name="mobileNumber"
+                                name="mobno"
                                 type="text"
                                 label="Mobile Number"
                                 placeholder="9999999999"
@@ -76,7 +83,7 @@ const Signup = () => {
                             />
 
                             <TextInput
-                                name="password"
+                                name="pwd"
                                 type="password"
                                 label="Password"
                                 placeholder="******"
@@ -84,7 +91,7 @@ const Signup = () => {
                             />
 
                             <TextInput
-                                name="confirmPassword"
+                                name="confirmpwd"
                                 type="password"
                                 label="Repeat Password"
                                 placeholder="******"
@@ -92,18 +99,8 @@ const Signup = () => {
                             />
 
                             <ButtonGroup>
-                                {!isSubmitting && (<StyledFormButton type="submit">Signup</StyledFormButton>)}
-
-                                {isSubmitting && (
-                                    <Rings 
-                                        type="ThreeDots"
-                                        color={colors.theme}
-                                        height={49}
-                                        width={100}
-                                    />
-                                )}
-
-                            <StyledFormButton type="reset">Reset</StyledFormButton>
+                                <StyledFormButton type="submit">Signup</StyledFormButton>
+                                <StyledFormButton type="reset">Reset</StyledFormButton>
                             </ButtonGroup>
                         </Form>
                     )}
